60b04c58c697ea771ca52c7035b88aa40ac9abc7
Dave Cole <dave@wistia.com>
2019-08-24 13:05:13 -0400

    Create Header and MainLayout components, to start creating some reusable structure

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,7 +1,20 @@
+import MainLayout from "../components/main-layout.jsx";
+import { css, jsx } from "@emotion/core";
+
+// this comment tells babel to convert jsx to calls to a function called jsx instead of React.createElement
+/** @jsx jsx */
+
+const aboutTextStyle = css`
+  width: 80%;
+  margin: 0 auto;
+`;
+
 const About = () => (
-  <p>
-    Repro is a tool for working together to reproduce and squash bugs.
-  </p>
+  <MainLayout>
+    <p css={aboutTextStyle}>
+      Repro is a tool for working together to reproduce and squash bugs.
+    </p>
+  </MainLayout>
 );
 
 export default About;
